c5f773e8cd7d4b75238cb6da43dc15dd7fa959e9
hyunho <mizzking75@gmail.com>
2023-01-19 20:13:07 +0900

    Initial Commit

---
+++ ScrumMaster/ScrumMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumMasterApp.swift
+//  ScrumMaster
+//
+//  Created by hyunho lee on 2023/01/19.
+//
+
+import SwiftUI
+
+@main
+struct ScrumMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
